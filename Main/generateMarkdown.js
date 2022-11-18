1a3ccf9c44d67965bfb76d057673354a2bd1ffea
“Nayonna <Nayonna Purnell>
2022-11-17 23:04:39 -0500

    added the license badge links

--- Main/generateMarkdown.js
+++ Main/generateMarkdown.js
@@ -1,20 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === 'MIT License'){
+    return `![MIT License](https://img.shields.io/badge/license-MIT-brightgreen)`
+  } else if (license === 'GNU GPLv3 License'){
+    return `![GNU GPLv3 License](https://img.shields.io/badge/license-GNU-brightgreen)`
+  } else if (license === 'Mozilla Public License 2.0'){
+    return `![Mozilla Public License 2.0](https://img.shields.io/badge/license-MPL-brightgreen)`
+  } else{
+    return `![Apache License 2.0](https://img.shields.io/badge/license-APACHE%202.0-brightgreen)`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license === 'MIT License'){
+    return `[MIT License](https://choosealicense.com/licenses/mit/)`
+  } else if (license === 'GNU GPLv3 License'){
+    return `[GNU GPLv3 License]( https://fsf.org/ )`
+  } else if (license === 'Mozilla Public License 2.0'){
+    return `[Mozilla Public License 2.0](http://mozilla.org/MPL/2.0/)`
+  } else{
+    return `[Apache License 2.0](http://www.apache.org/licenses/)`
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+ if(license === 'MIT License'){
+  return `A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+ } else if (license === 'GNU GPLv3 License'){
+  return `Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.`
+ } else if (license === 'Mozilla Public License 2.0'){
+  return `Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.`
+ } else {
+  return `A permissive license whose main conditions require preservation of copyright and license notices. Contributors provide an express grant of patent rights. Licensed works, modifications, and larger works may be distributed under different terms and without source code.`
+ }
+}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown({ projectTitle, projectDescription, installation, usage, license, contributors}) {
+  return `
+  # Project Title: ${projectTitle}
+  
+  ## Project Description
+  ${projectDescription}
+  
+  ## Setup/Installation Requirements
+  This is a great place to list setup instructions in a simple easy-to-understand format.
+  Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. 
+  Do I need to run a server? How should I set up my databases? Is there other code this application depends on? 
+  ${installation}
+  
+  ## Usage
+  Your usage documentation is probably the most important part of your README, without it very few people are going to trawl through your code to get it working.
+  Firstly we need to mention how to get hold of the code, whether it’s by cloning the repo or installing through a package manager. 
+  Don’t forget to link out to anything useful, to prevent anyone from getting stuck.
+  ${usage}
+  
+  ## License
+  ${renderLicenseSection(license)}
+  ${renderLicenseBadge(license)}
+  ${renderLicenseLink(license)}
+  
+  ## Contributors
+  This section should cover the basics and then link out to your CONTRIBUTING file if you have one.
+  This is also where you should add a link to your code of conduct, if you have one. 
+  You can make new contributors feel more comfortable, and assure them that any issues they have will be resolved. 
+  A good code of conduct to use is the Contributor Covenant.(https://www.contributor-covenant.org/)
+  ${contributors}
+  
+  ## Tests
+  `
 }
 
 module.exports = generateMarkdown;
--- Main/index.js
+++ Main/index.js
@@ -1,93 +1,6 @@
 const inquirer = require('inquirer');
 const { writeFile } = require('fs').promises;
-
-
-
-
-
-
-
-// // // TODO: Create a function to write README file
-// fs.writeToFile = ("genearateMarkdown.js", projectTitle, projectDescription,table_of_contents, github, email, (err) => {
-//   if(err)
-//   console.log(err);
-//   else {
-//     `
-// # Project Title: ${projectTitle}
-
-// ## Project Description
-// ${projectDescription}
-
-// ## Table of Contents
-// This section should list any major frameworks/libraries used to bootstrap your project. 
-// Leave any add-ons/plugins for the acknowledgements section. 
-// ${table_of_contents}
-
-// ## Getting Started
-// This is an example of how you may give instructions on setting up your project locally.
-// To get a local copy up and running follow these simple example steps.
-
-// ## Installation
-// A step by step guide that will tell you how to get the development environment up and running.
-
-// ## Usage
-
-// ## License
-
-// ## Contributing
-
-// ## Tests
-
-// ## Questions
-// ${github}
-// ${email}
-// `
-//   }
-// });
-
-
-// {
-//   type: 'input',
-//   name: 'table_of_contents',
-//   message: 'What is your Table of Contents?',
-// },
-// {
-//   type: 'input',
-//   name: 'installation',
-//   message: 'What do you need before installing the software?',
-// },
-// {
-//     type: 'input',
-//     name: 'usage',
-//     message: 'What do you need before installing the software?',
-//   },
-// {
-//     type: 'input',
-//     name: 'license',
-//     message: 'What do you need before installing the software?',
-// },
-// {
-//     type: 'input',
-//     name: 'contributing',
-//     message: 'What do you need before installing the software?',
-// },
-// {
-//     type: 'input',
-//     name: 'tests',
-//     message: 'What do you need before installing the software?',
-// },
-// {
-//     type: 'input',
-//     name: 'github',
-//     message: 'What is your github username?',
-// },
-// {
-//     type: 'input',
-//     name: 'email',
-//     message: 'What is your email?',
-// },
-// ])
-// }
+const generateMarkdown = require('./generateMarkdown.js');
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -111,42 +24,22 @@ const promptUser = () => {
   name: 'usage',
   message: 'List out any useful links to prevent your users from getting stuck.',
 },
+{
+  type: 'list',
+  name: 'license',
+  message: 'What license do you want for your project?',
+  choices: ['MIT License', 'GNU GPLv3 License', 'Mozilla Public License 2.0', 'Apache License 2.0'],
+  filter(val) {
+    return val.toLowerCase();
+  },
+},
+{
+  type: 'input',
+  name: 'contributors',
+  message: 'List the names of your contributors.',
+},
   ]);
 };
-
-const generateMarkdown = ({ projectTitle, projectDescription, installation, usage }) =>
-`
-# Project Title: ${projectTitle}
-
-## Project Description
-${projectDescription}
-
-## Setup/Installation Requirements
-This is a great place to list setup instructions in a simple easy-to-understand format.
-Leave nothing to chance! You want it to be easy for potential users, employers and collaborators to run your app. 
-Do I need to run a server? How should I set up my databases? Is there other code this application depends on? 
-${installation}
-
-## Usage
-Your usage documentation is probably the most important part of your README, without it very few people are going to trawl through your code to get it working.
-Firstly we need to mention how to get hold of the code, whether it’s by cloning the repo or installing through a package manager. 
-Don’t forget to link out to anything useful, to prevent anyone from getting stuck.
-${usage}
-
-## License
-You should add a copyright notice and a link to the license that your project is released under. 
-Without this information a lot of users, especially working in larger organisations, won’t be able to use your project.
-
-## Contributors
-This section should cover the basics and then link out to your CONTRIBUTING file if you have one.
-This is also where you should add a link to your code of conduct, if you have one. 
-You can make new contributors feel more comfortable, and assure them that any issues they have will be resolved. 
-A good code of conduct to use is the Contributor Covenant.(https://www.contributor-covenant.org/)
-${contributors}
-
-## Tests
-
-`
 
 const init = () => {
   promptUser()
